bdffa7dce0b16b9ca5d6a8555b242ad01fad9eda
Philip <pp@hjelmberg.se>
2021-12-06 18:23:41 +0100

    New change

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Heroes from './components/Heroes';
 import Nav from './components/Nav';
 import About from './components/About';
 import Home from './components/Home';
+import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React, {Fragment} from 'react';
+
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
             <Route path="/heroes" element={<Heroes />} />
             <Route path="/about" element={<About />} />
           </Routes>
+          <Footer />
         </Fragment>
       </Router>
     </div>
--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import '../App.css';
+import Philip from '../img/philip.jpg'
+import Elon from '../img/elon.jpg'
+import Jonathan from '../img/jonathan.jpg'
 
 function About() {
     return (
         <div className="about-wrapper">
-            <h1>About us</h1>
+            <h1>ABOUT US</h1>
                 <div className="friends">
-                    <div>Elon Månsson</div>
-                    <div>Jonathan Vigren</div>
-                    <div>Philip Hjelmberg</div>
+                    <div className="persons">
+                        <img src={Elon}></img>
+                        <h3>Elon Månsson</h3>
+                    </div>
+                    <div className="persons">
+                        <img src={Jonathan}></img>
+                        <h3>Jonathan Vigren</h3>
+                        </div>
+                    
+                    <div className="persons">
+                        <img src={Philip}></img>
+                        <h3>Philip Hjelmberg</h3>
+                    </div>
                 </div>
         </div>
     )
