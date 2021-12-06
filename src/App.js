f86b1ed83811369c138d802260bdc5bda221217f
Philip <pp@hjelmberg.se>
2021-12-06 11:07:09 +0100

    startpage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Heroes from './components/Heroes';
 import Nav from './components/Nav';
 import About from './components/About';
+import Home from './components/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React, {Fragment} from 'react';
 
@@ -14,7 +15,7 @@ function App() {
         <Fragment>
         <Nav />
           <Routes>
-            <Route path="/" element={<Heroes />} />
+            <Route path="/" element={<Home />} />
             <Route path="/heroes" element={<Heroes />} />
             <Route path="/about" element={<About />} />
           </Routes>
