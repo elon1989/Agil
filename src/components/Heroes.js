950a0a56b8ca8d727bfdd8f4eca0026aef7c152b
jonathanvigren <jonathan.saul.vigren@gmail.com>
2021-12-10 16:00:22 +0100

    e
    e

--- src/components/Heroes.js
+++ src/components/Heroes.js
@@ -1,70 +1,91 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import { useDispatch, useSelector } from "react-redux";
+import {changeURL, getGame} from './GameSlice'
 
 function Heroes(){
     const url = 'https://api.opendota.com/api/heroStats';
-    const [hero, setHero] = useState(null);
+    const [hero, setHero] = useState();
     const [selectedHero, setSelectedHero] = useState(1);
+    const [updatedArray, setUpdatedArray] = useState([]);
 
-
-
-    /* function selectHero(){
-    } */
-
+    
     useEffect(() => {
         axios.get(url)
         .then(response => {
             setHero(response.data)
-            
         })
     }, [url])
     
 
 
-    function SortAlphabetically() {
-        hero.sort((a, b) => a.localized_name.localeCompare(b.localized_name))
-        console.log()
+    
+    const SortAlphabetically = () => {
+        let newArray = hero.sort((a, b) => a.localized_name.localeCompare(b.localized_name));
+            setHero(newArray);
+            console.log(newArray)
     }
 
+
+    
     function SortWinRate() {
         hero.sort((a, b) => b.winrate - a.winrate)
-        console.log()
     }
 
-    let newArray = [];
+    function logThing() {
+        console.log(hero)
+    }
 
      function winRate() {
-         let newArray = hero.map((i) => Math.round(i.pro_win / i.pro_pick * 100));
+         let winArray = hero.map((i) => Math.round(i.pro_win / i.pro_pick * 100));
          for (let i = 0; i < hero.length; i++) {
-             hero[i].winrate = newArray[i];
+             hero[i].winrate = winArray[i];
             }
-         console.log(hero)
  }
+
+    const dispatch = useDispatch();
+
+    const handlegetGame = () => {
+        dispatch(getGame());
+    };
+
+    
 
     if(hero){
 
+       /* <button onClick={()=>handlegetGame()}></button>*/
         winRate();    
-    // Kommer att lägga till knappar med onclick för funktionerna sen    
-        SortAlphabetically();
+        // Kommer att lägga till knappar med onclick för funktionerna sen    
+      //  SortAlphabetically();
         SortWinRate();
 
         return (
             <>
-
+            <button className='sort_button' onClick={() => SortAlphabetically()}>Sort</button>
+            <button className='sort_button' onClick={()=>logThing()}>Log</button>
                 <div className="hero_wrapper">   
                     {hero.length ? hero.map((i) => {
                         if(selectedHero == i.id) {
                             return (
                             <div className={'hero_item' + ' ' + i.localized_name}>
-                                <h1>{i.localized_name}</h1>
-                                <div>{Math.round(i.pro_win / i.pro_pick * 100)}% Win rate</div> 
-                                <div className="heroImg"><img src={'https://steamcdn-a.akamaihd.net/' + i.img}/></div>
+                                <div className="heroleft">
+                                    <div>Base agi: {i.base_agi}</div>
+                                    <div>Base str: {i.base_str}</div>
+                                    <div>Base int: {i.base_int}</div>
+                                </div>
+                                <div className="herocenter">
+                                    <h1>{i.localized_name}</h1>
+                                    <div className="heroImg"><img className="heroPic" src={'https://steamcdn-a.akamaihd.net/' + i.img}/></div>
+                                </div>
+                                <div className="heroright">
+                                    <div>{i.winrate}% Win rate</div> 
+                                </div>
                             </div>)
                         } else {
                             return (
                             <div className={'herodiv' + ' ' + i.localized_name} onClick={()=>setSelectedHero(i.id)}>
                                 <h1>{i.localized_name}</h1>
-                                <div>{Math.round(i.pro_win / i.pro_pick * 100)}% Win rate</div> 
+                                <div>{i.winrate}% Win rate</div> 
                                 <div className="heroImg"><img src={'https://steamcdn-a.akamaihd.net/' + i.icon}/></div>
                             </div>)}
                         }): ''}
@@ -72,12 +93,13 @@ function Heroes(){
                 
             </>
         )
+    } else {
+        return (
+            <div>
+    
+            </div>
+        )
     }
-    return (
-        <div>
-
-        </div>
-    )
 }
 
 export default Heroes
