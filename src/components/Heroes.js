91af22b10f9b677aa1032e994a6dc3f67fe9e32d
jonathanvigren <jonathan.saul.vigren@gmail.com>
2021-12-13 16:37:11 +0100

    Update Heroes.js
    formatting

--- src/components/Heroes.js
+++ src/components/Heroes.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import { useDispatch, useSelector } from "react-redux";
-import {changeURL, getGame} from './GameSlice'
+import { changeURL, getGame } from './GameSlice'
 
-function Heroes(){
+function Heroes() {
     const url = 'https://api.opendota.com/api/heroStats';
     const [hero, setHero] = useState();
     const [selectedHero, setSelectedHero] = useState(1);
@@ -11,22 +11,22 @@ function Heroes(){
 
     useEffect(() => {
         axios.get(url)
-        .then(response => {
-            setHero(response.data)
-        }) 
+            .then(response => {
+                setHero(response.data)
+            })
     }, [url])
-    
-    
+
+
     const SortAlphabetically = () => {
         let newArray = hero.sort((a, b) => a.localized_name.localeCompare(b.localized_name));
-            setHero([...newArray]);
-            console.log(newArray)
+        setHero([...newArray]);
+        console.log(newArray)
     }
 
     const SortAlphabeticallyReverse = () => {
         let newArray = hero.sort((a, b) => b.localized_name.localeCompare(a.localized_name));
-            setHero([...newArray]);
-            console.log(hero)
+        setHero([...newArray]);
+        console.log(hero)
     }
 
     function SortWinRate() {
@@ -44,7 +44,7 @@ function Heroes(){
     }
 
     function SortLegs() {
-        let newArray = hero.sort((a, b) => b.legs - a.legs);       
+        let newArray = hero.sort((a, b) => b.legs - a.legs);
         setHero([...newArray]);
         console.log(hero);
     }
@@ -55,83 +55,85 @@ function Heroes(){
         console.log(hero)
     }
 
-     function winRate() {
-         let winArray = hero.map((i) => Math.round(i.pro_win / i.pro_pick * 100));
-         for (let i = 0; i < hero.length; i++) {
-             hero[i].winrate = winArray[i];
-            }
-            
-            console.log(hero)
- }
+
+    function winRate() {
+        let winArray = hero.map((i) => Math.round(i.pro_win / i.pro_pick * 100));
+        for (let i = 0; i < hero.length; i++) {
+            hero[i].winrate = winArray[i];
+        }
+
+        console.log(hero)
+    }
     const dispatch = useDispatch();
 
     const handlegetGame = () => {
         dispatch(getGame());
     };
 
-    
 
-    if(hero){
 
-       /* <button onClick={()=>handlegetGame()}></button>*/
-        winRate();  
+    if (hero) {
+
+        /* <button onClick={()=>handlegetGame()}></button>*/
+        winRate();
         console.log('test')
         console.log(hero);
 
         return (
             <>
-            <div className='sortSection'>
-            <div className='sortContainer'>
-            <button className='sort_button' onClick={() => SortAlphabetically()}>Sort ABC+</button>
-            <button className='sort_button' onClick={() => SortAlphabeticallyReverse()}>Sort ABC-</button>
-            </div>
-            <div className='sortContainer'>
-            <button className='sort_button' onClick={() => SortWinRate()}>Sort Winrate+</button>
-            <button className='sort_button' onClick={() => SortWinRateReverse()}>Sort Winrate-</button>
-            </div>
-            <div className='sortContainer'>
-            <button className='sort_button' onClick={() => SortLegs()}>Sort Legs+</button>
-            <button className='sort_button' onClick={() => SortLegsReverse()}>Sort Legs-</button>
-            </div>
-            </div>
-                <div className="hero_wrapper">   
+                <div className='sortSection'>
+                    <div className='sortContainer'>
+                        <button className='sort_button' onClick={() => SortAlphabetically()}>Sort ABC+</button>
+                        <button className='sort_button' onClick={() => SortAlphabeticallyReverse()}>Sort ABC-</button>
+                    </div>
+                    <div className='sortContainer'>
+                        <button className='sort_button' onClick={() => SortWinRate()}>Sort Winrate+</button>
+                        <button className='sort_button' onClick={() => SortWinRateReverse()}>Sort Winrate-</button>
+                    </div>
+                    <div className='sortContainer'>
+                        <button className='sort_button' onClick={() => SortLegs()}>Sort Legs+</button>
+                        <button className='sort_button' onClick={() => SortLegsReverse()}>Sort Legs-</button>
+                    </div>
+                </div>
+                <div className="hero_wrapper">
                     {hero.length ? hero.map((i) => {
-                        if(selectedHero == i.id) {
+                        if (selectedHero == i.id) {
                             return (
-                            <div className={'hero_item' + ' ' + i.localized_name}>
-                                <div className="heroleft">
-                                    <div>Base agi: {i.base_agi}</div>
-                                    <div>Base str: {i.base_str}</div>
-                                    <div>Base int: {i.base_int}</div>
-                                </div>
-                                <div className="herocenter">
-                                    <h1>{i.localized_name}</h1>
-                                    <div className="heroImg"><img className="heroPic" src={'https://steamcdn-a.akamaihd.net/' + i.img}/></div>
-                                    <h1>Legs: {i.legs}</h1>
-                                </div>
-                                <div className="heroright">
-                                    <div>{i.winrate}% Win rate</div> 
-                                </div>
-                            </div>)
-                        } else if (i.localized_name === 'Marci'){
+                                <div className={'hero_item' + ' ' + i.localized_name}>
+                                    <div className="heroleft">
+                                        <div>Base agi: {i.base_agi}</div>
+                                        <div>Base str: {i.base_str}</div>
+                                        <div>Base int: {i.base_int}</div>
+                                    </div>
+                                    <div className="herocenter">
+                                        <h1>{i.localized_name}</h1>
+                                        <div className="heroImg"><img className="heroPic" src={'https://steamcdn-a.akamaihd.net/' + i.img} /></div>
+                                        <h1>Legs: {i.legs}</h1>
+                                    </div>
+                                    <div className="heroright">
+                                        <div>{i.winrate}% Win rate</div>
+                                    </div>
+                                </div>)
+                        } else if (i.localized_name === 'Marci') {
                             return ''
                         } else {
                             return (
-                            <div className={'herodiv' + ' ' + i.localized_name} onClick={()=>setSelectedHero(i.id)}>
-                                <h1>{i.localized_name}</h1>
-                                <div>{i.winrate}% Win rate</div> 
-                                <div>Legs: {i.legs}</div>
-                                <div className="heroImg"><img src={'https://steamcdn-a.akamaihd.net/' + i.icon}/></div>
-                            </div>)}
-                        }): ''}
+                                <div className={'herodiv' + ' ' + i.localized_name} onClick={() => setSelectedHero(i.id)}>
+                                    <h1>{i.localized_name}</h1>
+                                    <div>{i.winrate}% Win rate</div>
+                                    <div>Legs: {i.legs}</div>
+                                    <div className="heroImg"><img src={'https://steamcdn-a.akamaihd.net/' + i.icon} /></div>
+                                </div>)
+                        }
+                    }) : ''}
                 </div>
-                
+
             </>
         )
     } else {
         return (
             <div>
-    
+
             </div>
         )
     }
