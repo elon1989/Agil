9f8aac88006e53a07bb32616ee9e12f43e6bba08
Philip <pp@hjelmberg.se>
2021-12-14 15:57:21 +0100

    About-Footer-Resp

--- src/components/About.js
+++ src/components/About.js
@@ -10,17 +10,29 @@ function About() {
             <h1>ABOUT US</h1>
                 <div className="friends">
                     <div className="persons">
-                        <img src={Elon}></img>
+                       <img src={Elon}></img>
                         <h3>Elon Månsson</h3>
+                        <p>Favorite hero: <p><span className="questionHighlight">Invoker - DOTA 2</span></p></p>
+                        <p>First MOBA ever played: <p><span className="questionHighlight">Dota</span></p></p>
+                        <p>Number of games: <p><span className="questionHighlight">200(?)</span></p></p>
+                        <p>Best gaming memory: <p><span className="questionHighlight">Beating a semipro in dota 2</span></p></p>
                     </div>
                     <div className="persons">
                         <img src={Jonathan}></img>
                         <h3>Jonathan Vigren</h3>
+                        <p>Favorite hero: <p><span className="questionHighlight">Enchantress - DOTA 2</span></p></p>
+                        <p>First MOBA ever played: <p><span className="questionHighlight">League of Legends</span></p></p>
+                        <p>Number of games: <p><span className="questionHighlight">3440</span></p></p>
+                        <p>Best gaming memory: <p><span className="questionHighlight">Idk mitt minne suger</span></p></p>
                         </div>
                     
                     <div className="persons">
                         <img src={Philip}></img>
                         <h3>Philip Hjelmberg</h3>
+                        <p>Favorite hero: <p><span className="questionHighlight">Lunara - Heroes of the Storm</span></p></p>
+                        <p>First MOBA ever played: <p><span className="questionHighlight">DOTA</span></p></p>
+                        <p>Number of games: <p><span className="questionHighlight">1200+</span></p></p>
+                        <p>Best gaming memory: <p><span className="questionHighlight">When i spoke to HeatoN on DH</span></p></p>
                     </div>
                 </div>
         </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -13,6 +13,7 @@ function Home() {
             <div className="left-column">
                 <div className="title">
                     <h1>LATEST NEWS FROM MOBA COMMUNITY</h1>
+                    
                 </div>
                 <div className="newsflow"> mi, commodo eget metus in, aliquet blandit dui. Nam iaculis diam hendrerit ultrices fermentum. Donec nunc orci, dapibus in ultricies id, pretium quis magna.</div>
             </div>
@@ -28,19 +29,21 @@ function Home() {
                     <h3>LOL STATS</h3>
                     <span className="small-button"><Link to='/heroes' className="navItem" element={Heroes}>Read more</Link></span>
                 </div>
-                <div className="stats">
-                    <h3>HoN STATS</h3>
-                    <span className="small-button"><Link to='/heroes' className="navItem" element={Heroes}>Read more</Link></span>
-                </div>
             </div>
             <div className="right-column">
             <div className="title">
                     <h1>LINKS</h1>
                 </div>
-                <img className="logos" src={Steam} alt=""></img><p>Steam</p>
-                <img className="logos" src={Dota} alt=""></img><p>DOTA 2</p>
-                <img className="logos" src={Hon} alt=""></img><p>Heroes of Newerth</p>
-                <img className="logos" src={Lol} alt=""></img><p>League of Legends</p>
+                <ul>
+                <li><img className="logos" src={Steam} alt=""></img></li>
+                <li><a className="linkName" href="https://store.steampowered.com/">Steam</a></li>
+                <li><img className="logos" src={Dota} alt=""></img></li>
+                <li><a className="linkName" href="https://store.steampowered.com/">DOTA 2</a></li>
+                <li><img className="logos" src={Hon} alt=""></img></li>
+                <li><a className="linkName" href="https://store.steampowered.com/">Hon</a></li>
+                <li><img className="logos" src={Lol} alt=""></img></li>
+                <li><a className="linkName" href="https://store.steampowered.com/">League of Legends</a></li>
+                </ul>
             </div>
         </div>
     )
